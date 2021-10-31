5578e597d6c114026166398252698c3be07ac61e
Leandro <leandroquiroga9514@gmail.com>
2021-10-31 19:02:52 -0300

    Create DB and Form validation

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,4 +1,55 @@
+const selector = (elment) => document.querySelector(elment);
+const creator = (element) => document.createElement(element);
+
+/* ================ Class ================ */
+class Interface {
+    showErr(text) {
+        let elementParent = selector('.time');
+        let div = creator('div');
+        div.classList.add('err');
+        div.textContent = text;
+
+        elementParent.appendChild(div)
+        setTimeout(() => {
+            div.remove()
+        }, 3500);
+    }
+}
+
+/* ================ Global Varialbes  ================ */
+const ui = new Interface();
+let form = selector('#form');
 let DB;
+let buttonOrder = selector('#calculator-mount')
+let client = {
+    table: '',
+    hours: '',
+    orden: [],
+}
+
+/* ================ Functions ================ */
+const newClient = () => {
+    let table = selector('#table').value;
+    let hours = selector('#hours').value;
+    let spinner = selector('.spinner');
+
+    // Check if there are empty fields
+    const empatyFields = [table, hours].some( field => field === '' );
+
+    if (empatyFields) {
+        ui.showErr('Todos los campos son obligatorios')
+        return
+    }
+
+    spinner.classList.remove('d-none')
+    client = { ...client, table, hours }
+    setTimeout(() => {
+        spinner.classList.add('d-none')
+        form.reset()
+        window.open('./../../facturacion.html', '_blank');
+     },2000)
+    console.log(client)
+}
 
 const createDB = () => {
     const database = window.indexedDB.open('sales', 1);
@@ -23,9 +74,9 @@ const createDB = () => {
     }
 }
 
-
 export const initAPP = () => {
     document.addEventListener('DOMContentLoaded', () => {
         createDB();
+        buttonOrder.addEventListener('click', newClient)
     })
 }
